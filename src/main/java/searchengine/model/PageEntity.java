10eae4f332944c55b9b40fb4ff406f47875e889e
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2024-02-05 14:37:27 +0300

    Added builder in PageEntity

--- src/main/java/searchengine/model/PageEntity.java
+++ src/main/java/searchengine/model/PageEntity.java
@@ -1,5 +1,6 @@
 package searchengine.model;
 
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
@@ -9,19 +10,20 @@ import org.hibernate.annotations.OnDeleteAction;
 import javax.persistence.*;
 
 @Entity
-@Table(name = "page")
+@Table(name = "page", indexes = @Index(columnList = "path", name = "path_index"))
 @Getter
 @Setter
 @EqualsAndHashCode(exclude = "id")
+@Builder
 public class PageEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "site_id", nullable = false, foreignKey = @ForeignKey(name = "FK_page_site"))
+    @JoinColumn(name = "site_id", referencedColumnName = "id", nullable = false, foreignKey = @ForeignKey(name = "FK_page_site"))
     @OnDelete(action = OnDeleteAction.CASCADE)
     private SiteEntity site;
-    @Column(columnDefinition = "text", nullable = false)
+    @Column(columnDefinition = "VARCHAR(255)", nullable = false)
     private String path;
     @Column(nullable = false)
     private int code;
