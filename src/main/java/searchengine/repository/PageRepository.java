404e539f1815005a444fa7f9d375278ad92963dd
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2024-02-05 14:40:23 +0300

    Added method for deleting pages

--- src/main/java/searchengine/repository/PageRepository.java
+++ src/main/java/searchengine/repository/PageRepository.java
@@ -1,7 +1,19 @@
 package searchengine.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import searchengine.model.PageEntity;
 
-public interface PageRepository extends JpaRepository<PageEntity, Long> {
+import javax.transaction.Transactional;
+
+public interface PageRepository extends JpaRepository<PageEntity, Integer> {
+
+    boolean existsByPath(String path);
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM PageEntity p WHERE p.site.id = :siteId")
+    void deleteBySiteId(@Param("siteId") Integer siteId);
 }
