afad2d7351cb7f1c22ed7cde35009143e7475bd6
Egor Alekseenko <alekseenko.ea@edu.spbstu.ru>
2024-02-19 10:32:54 +0300

    Replaced multi-imports with single imports

--- src/main/java/searchengine/model/PageEntity.java
+++ src/main/java/searchengine/model/PageEntity.java
@@ -7,7 +7,17 @@ import lombok.Setter;
 import org.hibernate.annotations.OnDelete;
 import org.hibernate.annotations.OnDeleteAction;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.ForeignKey;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "page", indexes = @Index(columnList = "path", name = "path_index"))
